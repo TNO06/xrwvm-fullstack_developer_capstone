fc778842109b24d2e01ede09e245f01659ea4b01
TNO06 <dhawalkashyap5399@gmail.com>
2025-11-06 02:22:57 -0500

    hi

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,15 @@
-import LoginPanel from "./components/Login/Login"
-import { Routes, Route } from "react-router-dom";
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register"; // <-- THE FIX: Add this line
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<Register />} /> {/* <-- This will now work */}
     </Routes>
   );
 }
+
 export default App;
